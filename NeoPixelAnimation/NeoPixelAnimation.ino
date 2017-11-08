ac03154cd89245a397b0358e59421f328719317f
Stheffany Hadlich <tefani.hadlich@gmail.com>
2017-11-08 15:05:43 -0200

    Display shows time

--- NeoPixelAnimation/NeoPixelAnimation.ino
+++ NeoPixelAnimation/NeoPixelAnimation.ino
@@ -4,7 +4,7 @@
 #endif
 
 #define PIN            2
-#define NUMPIXELS      4ff
+#define NUMPIXELS      2
 int brilho = 10;
 #define BRIGHTNESS brilho
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -15,114 +15,23 @@ int LED;
 void setup(){
   Serial.begin(9600);
   pixels.setBrightness(BRIGHTNESS);
+  pixels.setPixelColor(0, pixels.Color(0,0,0));
+  pixels.setPixelColor(0, pixels.Color(0,0,0));
   pixels.begin();
   pixels.show();
   
 }
 
-void teclasindividuais(){
-      LED = 0;
-      LED = 1;            
-      LED = 2;
-      LED = 3;
-
-  
-    pixels.setPixelColor(LED, pixels.Color(255,0,0));
-    pixels.show();
- 
-    pixels.setPixelColor(LED, pixels.Color(0,255,0));
-    pixels.show();        
-  
-    pixels.setPixelColor(LED, pixels.Color(0,0,255));
-    pixels.show();        
-  
-    pixels.setPixelColor(LED, pixels.Color(0,0,0));
-    pixels.show();        
-
-}
-
-void pisca(){
-
-
-  pixels.setPixelColor(0, pixels.Color(0,0,0));
-  pixels.setPixelColor(1, pixels.Color(0,0,0));
-  pixels.setPixelColor(2, pixels.Color(0,0,0));
-  pixels.setPixelColor(3, pixels.Color(0,0,0));  
-  pixels.show();
-  delay(1000);
-
-for (int i = 0; i < 6; i++) {
-    pixels.setPixelColor(0, pixels.Color(255,0,0));
-    pixels.setPixelColor(1, pixels.Color(0,0,0));
-    pixels.setPixelColor(2, pixels.Color(0,0,0));
-    pixels.setPixelColor(3, pixels.Color(0,0,0));     
-    pixels.show();
-    delay(100);    
-    pixels.setPixelColor(0, pixels.Color(0,255,0));
-    pixels.setPixelColor(1, pixels.Color(255,0,0));
-    pixels.setPixelColor(2, pixels.Color(0,0,0));
-    pixels.setPixelColor(3, pixels.Color(0,0,0));     
-    pixels.show(); 
-    delay(100);    
-    pixels.setPixelColor(0, pixels.Color(0,0,255));
-    pixels.setPixelColor(1, pixels.Color(0,255,0));
-    pixels.setPixelColor(2, pixels.Color(255,0,0));
-    pixels.setPixelColor(3, pixels.Color(0,0,0));     
-    pixels.show();
-    delay(100);
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.setPixelColor(1, pixels.Color(0,0,255));
-    pixels.setPixelColor(2, pixels.Color(0,255,0));
-    pixels.setPixelColor(3, pixels.Color(255,0,0));    
-    pixels.show();
-    delay(100);
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.setPixelColor(1, pixels.Color(0,0,0));
-    pixels.setPixelColor(2, pixels.Color(0,0,255));
-    pixels.setPixelColor(3, pixels.Color(0,255,0));    
-    pixels.show();
-    delay(100);
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.setPixelColor(1, pixels.Color(0,0,0));
-    pixels.setPixelColor(2, pixels.Color(0,0,0));
-    pixels.setPixelColor(3, pixels.Color(0,0,255));    
-    pixels.show();
-    delay(100);
-}
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.setPixelColor(1, pixels.Color(0,0,0));
-    pixels.setPixelColor(2, pixels.Color(0,0,0));
-    pixels.setPixelColor(3, pixels.Color(0,0,0));  
-    pixels.show();
-
-}
-
-void aumBrilho(){
-  if(brilho < 10){
-    brilho = brilho + 1;
-  }else if(brilho < 250){
-    brilho = brilho + 10;
-  }else if(brilho < 255){
-    brilho = brilho + 1;
-  }
-  pixels.show();
-}
-
-void baiBrilho(){
-  if(brilho > 250){
-    brilho = brilho - 1;
-  }else if(brilho > 10){
-    brilho = brilho - 10;    
-  }else if(brilho > 1){
-    brilho = brilho - 1;    
-  }
-  pixels.show();
-}
-
 void loop() {
 
-pixels.setBrightness(brilho);
+pixels.setPixelColor(0, pixels.Color(0,0,255));
+pixels.setPixelColor(1, pixels.Color(255,0,0));
+pixels.show();
+delay(500);
 
-pisca();
-  
+pixels.setPixelColor(0, pixels.Color(0,0,0));
+pixels.setPixelColor(1, pixels.Color(0,0,0));
+pixels.show();
+delay(1000);
+
 }
--- SpoonfulOfSugar/SpoonfulOfSugar.ino
+++ SpoonfulOfSugar/SpoonfulOfSugar.ino
@@ -16,11 +16,50 @@
 #define D9 3
 #define D10 1
 
+// -------------------------------------------- OLED RELATED STUFF ----------------------
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET LED_BUILTIN
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 
 
 // ------------------------------------------ WEB RELATED FUNCTIONS ------------------------------------------------------------
-#define ssid ""
-#define password ""
+#define ssid "NDakota"
+#define password "stheffany"
 WiFiServer server(80);
 strDateTime dateTime;
 NTPtime NTPch("br.pool.ntp.org");
@@ -57,6 +96,21 @@ void startServer(void)
 
 
 // ----------------------------------------------------------------------- TIME RELATED FUNCTIONS -------------------------------------------------------------
+
+
+void ScreenTime(){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print(hour());
+  display.print(":");
+  display.print(minute());
+  display.print(":");
+  display.print(second());
+  display.display();
+  delay(500);
+  display.clearDisplay();  
+}
 
 void printDigits(int digits)
 {
@@ -334,6 +388,11 @@ void setup() {
   WifiConnection();
   startServer();
   MedicineInitialize();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 }
 
 
@@ -350,5 +409,6 @@ void loop() {
  
   IsThereAlarm();
   digitalClockDisplay();  
-  delay(1000);
+  ScreenTime();
+  delay(500);
 }
